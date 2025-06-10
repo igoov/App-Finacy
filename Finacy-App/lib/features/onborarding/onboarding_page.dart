5a998742b53eef536478a072c0a60fc0950a9323
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-06-10 01:06:19 -0300

    Adicionado a opção de entrar, e começar e um texto na tela.

--- Finacy-App/lib/features/onborarding/onboarding_page.dart
+++ Finacy-App/lib/features/onborarding/onboarding_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/features/splash/common/constants/app_colors.dart';
+import 'package:flutter_application_1/features/splash/common/constants/app_text_styles.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
@@ -7,24 +8,47 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 60.0),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: AppColors.iceWhite,
-              child: Image.asset(
-                'assets/images/Group1.png',
+      body: Align(
+        child: Column(
+          children: [
+            const SizedBox(height: 60.0),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: AppColors.iceWhite,
+                child: Image.asset(
+                  'assets/images/Group1.png',
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: AppColors.white,
+            Expanded(
+              child: Container(
+                color: AppColors.white
+              ),
             ),
-          ),
-        ],
+            Text(
+              'Gaste com mais inteligência',
+              style: AppTextStyles.mediumText.copyWith(
+                color: AppColors.greeligthTwo,
+              ),
+            ),
+            Text(
+              'E economize mais',
+              style: AppTextStyles.mediumText.copyWith(
+                color: AppColors.greeligthTwo,
+              ),
+            ),
+            ElevatedButton(onPressed: (){}, child: const Text('começar'),
+            ),
+            Text(
+              'Já tem conta? Entrar',
+              style: AppTextStyles.smallText.copyWith(
+                color: AppColors.grey,
+              ),
+            ),
+            SizedBox(height: 60.0,)
+          ],
+        ),
       ),
     );
   }
--- Finacy-App/lib/features/splash/common/constants/app_colors.dart
+++ Finacy-App/lib/features/splash/common/constants/app_colors.dart
@@ -8,5 +8,6 @@ class AppColors {
   static const List<Color> greenGradient = [greeligthOne, greeligthTwo];
   static const Color white = Color(0xFFFFFFFF);
   static const Color iceWhite = Color(0xFFEEF8F7);
+  static const Color grey = Color(0xFF444444);
 }
   
